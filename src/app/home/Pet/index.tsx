54816de200a0e78e16b094f3134c8d0efa6a8f68
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-08-16 15:13:09 -0700

    Style update

--- src/app/home/Pet/index.tsx
+++ src/app/home/Pet/index.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export function Pet() {
-  return <div className="h-48 w-48 bg-neutral-500">Pet goes here</div>;
+  return <div className="h-72 w-72 bg-neutral-300">Pet goes here</div>;
 }
