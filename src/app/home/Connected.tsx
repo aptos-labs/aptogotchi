776af74f3cddbfb814a91c666e8a4aff463f92e0
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-08-18 07:33:07 -0700

    Update after mint

--- src/app/home/Connected.tsx
+++ src/app/home/Connected.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Pet } from "./Pet";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { Network, Provider } from "aptos";
@@ -13,45 +13,46 @@ export function Connected() {
   const [isLoading, setIsLoading] = useState(true);
   const { account, network } = useWallet();
 
-  useEffect(() => {
-    if (!account || !network) return;
+  const fetchPet = useCallback(async () => {
+    if (!account?.address) return;
 
-    const fetchData = async () => {
-      // build a transaction payload to be submitted
-      const payload = {
-        function:
-          "0xe8a7346fd49d2aeaa1ddd1c58c865eeb617c4f88870c8b0a25cfae2938eb5574::main::get_aptogotchi",
-        type_arguments: [],
-        arguments: [account.address],
-      };
-
-      const response = await provider.view(payload);
-      const noPet = ["", "0", "0", "0"];
-
-      // if no pet exists, show minting component
-      if (JSON.stringify(response) !== JSON.stringify(noPet)) {
-        console.log("RESPONSE: ", JSON.stringify(response));
-        console.log("NO PET: ", JSON.stringify(noPet));
-        console.log("PET: ", pet);
-        // get and set pet data from user's wallet
-        setPet({
-          name: response[0] as unknown as string,
-          health_points: response[2] as unknown as number,
-          happiness: response[3] as unknown as number,
-        });
-      }
-      setIsLoading(false);
+    // build a transaction payload to be submitted
+    const payload = {
+      function:
+        "0xe8a7346fd49d2aeaa1ddd1c58c865eeb617c4f88870c8b0a25cfae2938eb5574::main::get_aptogotchi",
+      type_arguments: [],
+      arguments: [account.address],
     };
 
-    fetchData();
-  }, []);
+    setIsLoading(true);
+
+    const response = await provider.view(payload);
+    const noPet = ["", "0", "0", "0"];
+
+    // if no pet exists, show minting component
+    if (JSON.stringify(response) !== JSON.stringify(noPet)) {
+      // get and set pet data from user's wallet
+      setPet({
+        name: response[0] as unknown as string,
+        health_points: response[2] as unknown as number,
+        happiness: response[3] as unknown as number,
+      });
+    }
+    setIsLoading(false);
+  }, [account?.address]);
+
+  useEffect(() => {
+    if (!account?.address || !network) return;
+
+    fetchPet();
+  }, [account?.address, fetchPet, network]);
 
   // TODO: Design a nice loading screen
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col gap-3 p-3">
-      {pet ? <Pet pet={pet} /> : <Mint />}
+      {pet ? <Pet pet={pet} /> : <Mint fetchPet={fetchPet} />}
     </div>
   );
 }
