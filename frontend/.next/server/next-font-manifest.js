97c748b1c502a692c6ffb359b733768713e8beea
darren wang <darren@aptoslabs.com>
2023-08-30 18:32:09 -0700

    remove .next & add to gitignore

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/darren/Desktop/Projects/aptogotchi/frontend/src/app/layout\":[\"static/media/03f4cb0148379bc4-s.p.ttf\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
