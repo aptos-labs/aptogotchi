4a3d25118690bc3c4879f5da47217014b17f3834
Aiwei Wu <aiweiwu@Aiweis-MacBook-Pro.local>
2023-11-02 10:15:57 -0700

    revert back to master

--- frontend/src/components/HealthBar/index.tsx
+++ frontend/src/components/HealthBar/index.tsx
@@ -6,8 +6,6 @@ export interface HealthBarProps {
   icon: "heart" | "star";
 }
 
-export const BASE_PATH = "/assets/";
-
 export function HealthBar({
   totalHealth,
   currentHealth,
@@ -16,11 +14,9 @@ export function HealthBar({
   const fullIcons = Math.floor(currentHealth / 2);
   const halfIcons = currentHealth % 2;
   const emptyIcons = Math.floor((totalHealth - currentHealth) / 2);
-  const apple = BASE_PATH + "apple-1.png";
 
   return (
-    <div>
-      <div className="flex flex-wrap gap-1">
+    <div className="flex flex-wrap gap-1">
         {range(fullIcons).map((i) => (
           <i key={i} className={`nes-icon ${icon}`} />
         ))}
@@ -30,13 +26,6 @@ export function HealthBar({
         {range(emptyIcons).map((i) => (
           <i key={i} className={`nes-icon is-empty ${icon}`} />
         ))}
-      </div>
-      {/* <div className="flex flex-wrap gap-1">
-              {range(fullIcons).map((i) => (
-          // <i key={i} className={`nes-icon ${icon}`} />
-          <img key={i} className="nes-avatar" alt="Apple icon" src={apple}></img>
-        ))}
-      </div> */}
     </div>
   );
 }
