e4172b210b1c99f977a2bfe483284c4a4671406d
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-01-19 11:55:56 -0800

    use placeholder value for profile

--- move/sh_scripts/move_publish.sh
+++ move/sh_scripts/move_publish.sh
@@ -4,7 +4,10 @@ set -e
 
 echo "##### Publishing module #####"
 
-PROFILE=testnet-5
+# Profile is the account you used to execute transaction
+# Run "aptos init" to create the profile, then get the profile name from .aptos/config.yaml
+PROFILE=to_fill
+
 ADDR=0x$(aptos config show-profiles --profile=$PROFILE | grep 'account' | sed -n 's/.*"account": \"\(.*\)\".*/\1/p')
 
 aptos move publish \
--- move/sh_scripts/move_run_script_feed_gotchi.sh
+++ move/sh_scripts/move_run_script_feed_gotchi.sh
@@ -4,7 +4,10 @@ set -e
 
 echo "##### Running move script to feed gotchi #####"
 
-PROFILE=testnet-5
+# Profile is the account you used to execute transaction
+# Run "aptos init" to create the profile, then get the profile name from .aptos/config.yaml
+PROFILE=to_fill
+
 ADDR=0x$(aptos config show-profiles --profile=$PROFILE | grep 'account' | sed -n 's/.*"account": \"\(.*\)\".*/\1/p')
 
 # Need to compile the package first
