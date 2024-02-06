c25b1e8af2bb810cd9ebe00c4eebdc44d4534523
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-02-05 18:27:37 -0800

    add comment

--- move/sh_scripts/move_publish.sh
+++ move/sh_scripts/move_publish.sh
@@ -6,10 +6,12 @@ echo "##### Publishing module #####"
 
 # Profile is the account you used to execute transaction
 # Run "aptos init" to create the profile, then get the profile name from .aptos/config.yaml
-PROFILE=testnet-1
+PROFILE=to_fill
 
 ADDR=0x$(aptos config show-profiles --profile=$PROFILE | grep 'account' | sed -n 's/.*"account": \"\(.*\)\".*/\1/p')
 
+# You need to checkout to randomnet branch in aptos-core and build the aptos cli manually
+# This is a temporary solution until we have a stable release randomnet cli
 ~/go/src/github.com/aptos-labs/aptos-core/target/debug/aptos move publish \
 	--assume-yes \
   --profile $PROFILE \
--- move/sh_scripts/move_tests.sh
+++ move/sh_scripts/move_tests.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "##### Running tests #####"
 
+# You need to checkout to randomnet branch in aptos-core and build the aptos cli manually
+# This is a temporary solution until we have a stable release randomnet cli
 ~/go/src/github.com/aptos-labs/aptos-core/target/debug/aptos move test \
   --package-dir move \
   --dev
