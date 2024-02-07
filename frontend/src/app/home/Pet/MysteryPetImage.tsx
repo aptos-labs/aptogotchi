3eb07dcd90d3fc656fbdb01a1233f1db7ca5869c
darren wang <darren@aptoslabs.com>
2024-02-06 18:29:29 -0800

    add giftbox

--- frontend/src/app/home/Pet/MysteryPetImage.tsx
+++ frontend/src/app/home/Pet/MysteryPetImage.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { MysteryBoxImage } from "./Image";
+import Lottie from "react-lottie";
+import animationData from "./mystery-box.json";
 
 export function MysteryPetImage() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="flex flex-col gap-6 self-center">
-      <MysteryBoxImage />
+      <Lottie options={defaultOptions} height={400} width={400} />
     </div>
   );
 }
