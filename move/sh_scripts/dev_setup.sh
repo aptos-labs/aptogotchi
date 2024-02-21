bcdb316be290f063019c7833d0107e67241d835e
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-02-21 12:03:38 -0800

    remove cli hash check

--- move/sh_scripts/dev_setup.sh
+++ move/sh_scripts/dev_setup.sh
@@ -11,10 +11,8 @@ if ! command -v aptos &>/dev/null; then
     echo "aptos could not be found"
     echo "installing it..."
     TARGET=Ubuntu-x86_64
-    VERSION=2.3.0
+    VERSION=2.4.0
     wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v$VERSION/aptos-cli-$VERSION-$TARGET.zip
-    sha=$(shasum -a 256 aptos-cli-$VERSION-$TARGET.zip | awk '{ print $1 }')
-    [ "$sha" != "d9dc5c6ab3366f25d8547ca939f4a6be659f064c344a81f14631bc435ef6bafe" ] && echo "shasum mismatch" && exit 1
     unzip aptos-cli-$VERSION-$TARGET.zip
     chmod +x aptos
 else
